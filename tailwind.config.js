db9aaa3345aef65de0cdcf79c901c709655eda68
ndevil0217 <kjav.spt@gmail.com>
2024-02-05 10:12:24 +0100

    feat: tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        screens: {
+          "3xl": "1920px",
+        },
+      },
+      fontSize: {
+        sm: "14px",
+        md: "16px",
+        lg: ["20px", "normal"],
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#204ECF",
+          800: "#1D5BD6",
+          950: "#0400D0",
+        },
+        purple: {
+          500: "#524FFF",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addVariant }) {
+      addVariant("child", "& > *")
+      addVariant("child-hover", "& > *:hover")
+    },
+  ],
 }
